a64a15569b7253a483d15eef504f45ab0e436ef2
RajatR1152 <121629970+RajatR1152@users.noreply.github.com>
2023-09-01 20:08:43 +0530

    Update Contact.js

--- cars_finder/components/Contact.js
+++ cars_finder/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div style={{ 'backgroundImage': `url('https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29udGFjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60')` }} className="container w-full my-5 p-20 bg-cover bg-no-repeat h-screen">
+        <div style={{ 'backgroundImage': `url('https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29udGFjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60')` }} className="container md:block hidden w-full my-5 p-20 bg-cover bg-no-repeat h-screen">
             <h1 className="text-white text-4xl w-6/12">Buying a new car ?</h1>
             <h2 className="text-white text-xl my-10 w-7/12">contact us we provide you a wide range of new and used cars availabe in your city.</h2>
             <div className="container inherit opacity-80 bg-gray-800 p-5 w-5/12">
